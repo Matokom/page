763b3712f1577aab964811a36882be3a2b27ba78
Matokom <94479110+Matokom@users.noreply.github.com>
2021-11-23 16:42:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if language == "Python":
     st.markdown("""Simulačný model priemyselného robota je vytvorený v module určenom na vizualizáciu - **PyVista**. 
     Cieľom bolo vytvoriť prostredie, v ktorom bude priemyselný robot riadený do jednotlivých targetov využitím hlasových
     príkazov. Video:""")
-    vid_file1 = open("media\ovladany_hlasom_low.mp4", 'rb').read()
+    vid_file1 = open("ovladany_hlasom_low.mp4", 'rb').read()
     st.video(vid_file1)
 
 if language == "Python":
@@ -25,28 +25,28 @@ if language == "Python":
     st.subheader("Mobilný robot v bludisku")
     st.markdown("""Simulačný model malého mobilného robota - micromouse a jeho ovládanie pomocou šípok na klávesnici.
 Aktuálne sa na tomto projekte pracuje. Video:""")
-    vid_file1 = open("media\labyrint.gif", 'rb').read()
+    vid_file1 = open("labyrint.gif", 'rb').read()
     st.video(vid_file1)
 
 if language == "Python":
     st.subheader("")
     st.subheader("Model PR ABB")
     st.markdown("Znázornenie prierezu robota v priestore spolu s jeho súsradnicovými systémami v jednotlivých kĺboch.")
-    img = Image.open("media\pr2.jpg")
+    img = Image.open("pr2.jpg")
     st.image(img, caption="PR v Pythone")
 
 if language == "Python":
     st.subheader("")
     st.subheader("Sieťový model PR")
     st.markdown("Na obrázku nižšie je znázornený trojuholníková sieť reprezentujúca 3D model PR.")
-    img = Image.open("media\pr1.jpg")
+    img = Image.open("pr1.jpg")
     st.image(img, caption="Sieť PR")
 
 if language == "Matlab":
     st.subheader("Kuka riadená neurónovou sieťou")
     st.markdown("""Na videu môžete vidieť 7 kĺbový priemyselný robot Kuka, ktorý je do 4 zvolených targetov
 riadený pomocou natrénovanej neurónovej siete. Sieť bola trénovaná evolučnými algoritmami. Video:""")
-    vid_file = open("media\Kukalow.wmv", 'rb').read()
+    vid_file = open("Kukalow.wmv", 'rb').read()
     st.video(vid_file)
 
 if language == "Matlab":
@@ -54,7 +54,7 @@ if language == "Matlab":
     st.subheader('Mobilný robot v bludisku.')
     st.markdown("""Malý mobilný robot, ktorý chodí po bludisku a ktorého vizualizácia je upravená z dôvodu vyššieho 
 výpočtového výkonu. Pri kolízii sa robot sfarbí na červeno. Video:""")
-    vid_file4 = open("media\mybludiskolow.mp4", 'rb').read()
+    vid_file4 = open("mybludiskolow.mp4", 'rb').read()
     st.video(vid_file4)
 
 if language == "Matlab":
@@ -62,7 +62,7 @@ if language == "Matlab":
     st.subheader('Konfigurácia robota vyžitím GA')
     st.markdown("""Na videu nižšie môžete vidieť obálku priemyselného robota s prekážkou a požadovaným targetom. Cieľom
 bolo vytvoriť kinematiku priemyselného robota a pomocou genetických algoritmov nájsť vhodnú konfiguráciu. Video:""")
-    vid_file1 = open("media\obalkalow.mp4", 'rb').read()
+    vid_file1 = open("obalkalow.mp4", 'rb').read()
     st.video(vid_file1)
 
 if language == "Matlab":
@@ -70,11 +70,11 @@ if language == "Matlab":
     st.subheader('Hľadanie najkratšej dráhy v 2D priestore s prekážkou')
     st.markdown("""Program na hľadanie najkratšej dráhy v 2D priestore s prekážkou. Znázornené písmenka nám reprezentujú
 obchádzanú prekážku. Dráha je vytvorená z desiatich bodov a ich sponicami. Video:""")
-    vid_file = open("media\lowobchadzanie.mp4", 'rb').read()
+    vid_file = open("lowobchadzanie.mp4", 'rb').read()
     st.video(vid_file)
 
 if language == "Matlab":
     st.subheader('')
     st.subheader('Kinematika mobilného robota')
-    img = Image.open("media\PR_Matlab_Kinematika.jpg")
+    img = Image.open("PR_Matlab_Kinematika.jpg")
     st.image(img, caption="PR v Pythone")
